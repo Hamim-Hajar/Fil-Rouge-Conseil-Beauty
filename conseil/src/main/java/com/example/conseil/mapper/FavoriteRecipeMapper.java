a61c647f7b0405e4debcb5527bcec003a13dc74a
hajarhjr <hamimhajar85@gmail.com>
2024-10-14 19:31:37 +0100

    a commit

--- conseil/src/main/java/com/example/conseil/mapper/FavoriteRecipeMapper.java
+++ conseil/src/main/java/com/example/conseil/mapper/FavoriteRecipeMapper.java
@@ -1,17 +1,17 @@
-package com.example.conseil.mapper;
-
-import com.example.conseil.dto.FavoriteRecipeDto;
-import com.example.conseil.entities.FavoriteRecipe;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-@Mapper(componentModel = "spring")
-public interface FavoriteRecipeMapper {
-    @Mapping(source = "recipe.id", target = "recipeId")
-    @Mapping(source = "visiteur.id", target = "visiteurId")
-    FavoriteRecipeDto toDto(FavoriteRecipe favoriteRecipe);
-
-    @Mapping(source = "recipeId", target = "recipe.id")
-    @Mapping(source = "visiteurId", target = "visiteur.id")
-    FavoriteRecipe toEntity(FavoriteRecipeDto favoriteRecipeDto);
-}
+//package com.example.conseil.mapper;
+//
+//import com.example.conseil.dto.FavoriteRecipeDto;
+//import com.example.conseil.entities.FavoriteRecipe;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//
+//@Mapper(componentModel = "spring")
+//public interface FavoriteRecipeMapper {
+//    @Mapping(source = "recipe.id", target = "recipeId")
+//    @Mapping(source = "visiteur.id", target = "visiteurId")
+//    FavoriteRecipeDto toDto(FavoriteRecipe favoriteRecipe);
+//
+//    @Mapping(source = "recipeId", target = "recipe.id")
+//    @Mapping(source = "visiteurId", target = "visiteur.id")
+//    FavoriteRecipe toEntity(FavoriteRecipeDto favoriteRecipeDto);
+//}
